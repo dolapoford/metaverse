c9a7159ce86abee4b5820986b5548b1b88d8420a
Ahmad Dolapo Asiyanbola <dolapo@fordsoft.tech>
2023-09-03 12:22:32 +0100

    second commit

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -1,10 +1,11 @@
 'use client';
 
-
 import { motion } from 'framer-motion';
 
 import styles from '../styles';
+
 import { fadeIn } from '../utils/motion';
+
 const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
   <motion.div
     variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
